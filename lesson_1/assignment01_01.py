22aa4c51d8b8e7310530329a2e23923184a145cf
Andrii Holub <andrii.holub@lemberg.co.ua>
2022-07-15 08:58:46 +0300

    init commit

---
+++ lesson_1/assignment01_01.py
@@ -0,0 +1,10 @@
+#!py -3
+
+# Assignment 01-01
+#
+# Assign actual data to the variables below
+
+name = "Brian"
+
+print("My name is {name}".format(name=name))
+
---
+++ lesson_2/assignment02_01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# Assignment 02-01
+#
+# Task: Implement cylinder volume formula:
+#
+# Formula: V=pi*r*r*h, where
+#
+#          V - volume of cylinder
+#          pi - pi math constant
+#          r - radius of the circular base
+#          h - height of cylinder
+
+pi = 3.14
+r = 
+h =
+v =
+
+print(f"Volume of cylinder with radius r={r} and height h={h}: {V}") 
