19976c9a2b9712356df00a28a38c835650608357
Andrii Holub <andrii.holub@lemberg.co.ua>
2022-08-06 19:23:27 +0300

    fix assigment06_01

--- lesson_6/assignment06_01.py
+++ lesson_6/assignment06_01.py
@@ -14,49 +14,28 @@
 # 3 скільки разів зустрічається слово в реченні
 # 4 порахувати статистику використаних літер
 
+
 text = input('Введіть речення: ')
-# text: str = 'aaaaaaasdf dfg fgh asdf f d p p p p f'
-word_list = text.split(' ')
+
+word_list = text.split()
 
 stat_dict = {'Number of words': len(word_list), 'Number of unique words': len(set(word_list))}
 
+unique_charachter_list = []
+letter_occurs_dict = {}
+
 word_occurs_dict = {}
 for word in word_list:
-	if word not in word_occurs_dict.keys():
+	if word not in word_occurs_dict:
 		word_occurs_dict[word] = f'{text.count(word)} times'
+	for i in word:
+		if i not in unique_charachter_list:
+			unique_charachter_list.append(i)
+		if i not in letter_occurs_dict:
+			letter_occurs_dict[i] = f'{unique_charachter_list.count(i)} times'
+  
 stat_dict['Word occurs'] = word_occurs_dict
 
-letter_occurs_dict = {}
-string_of_letters = text.replace(' ', '')
-unique_letters_list = []
-
-for i in string_of_letters:
-	if i.isalpha():
-		unique_letters_list.append(i)
-
-for i in unique_letters_list:
-	if i not in letter_occurs_dict.keys():
-		letter_occurs_dict[i] = f'{unique_letters_list.count(i)} times'
 stat_dict['Letter occurs'] = letter_occurs_dict
 
 print(stat_dict)
-
-# Input
-# 'aaaaaaasdf dfg fgh asdf f d p p p p f'
-# Output
-# {
-# 	'Number of words': 11,
-# 	'Number of unique words': 7,
-# 	'Word occurs': {
-# 		'aaaaaaasdf': '1 times',
-# 		'dfg': '1 times',
-# 		'fgh': '1 times',
-# 		'asdf': '2 times',
-# 		'f': '6 times',
-# 		'd': '4 times',
-# 		'p': '4 times'
-# 	},
-# 	'Letter occurs': {
-# 		'a': '8 times', 's': '2 times', 'd': '4 times', 'f': '6 times', 'g': '2 times', 'h': '1 times', 'p': '4 times'
-# 	}
-# }
