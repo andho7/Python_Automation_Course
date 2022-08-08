bde4e9a97fba42c151e40a6d1ebed2f298032d0c
Andrii Holub <andrii.holub@lemberg.co.ua>
2022-08-08 09:30:39 +0300

    fix assigment07_06

--- lesson_7/assignment07_06.py
+++ lesson_7/assignment07_06.py
@@ -1,25 +1,30 @@
 """Задача: написати функцію пошуку елемента у послідовності.
 
-   Деталі:
-            * функція приймає два аргумента - послідовність та елемен
-            * функція повинна повернути індекс елемента у послідовності
-              або -1 якщо не знайдено
-            * це аналог функції str.find, list.index
-            * написати якийсь код який надає пару прикладів використування
+	 Деталі:
+						* функція приймає два аргумента - послідовність та елемен
+						* функція повинна повернути індекс елемента у послідовності
+							або -1 якщо не знайдено
+						* це аналог функції str.find, list.index
+						* написати якийсь код який надає пару прикладів використування
 
-    Приклад:
-            * search_linearly([1, 8, 7, 33, 9, 2], 8) -> повертає 1
-            * search_linearly("hello world!", "!") -> повертає 11
-            * search_linearly(tuple(range(10)), 10) -> повертає -1
+		Приклад:
+						* search_linearly([1, 8, 7, 33, 9, 2], 8) -> повертає 1
+						* search_linearly("hello world!", "!") -> повертає 11
+						* search_linearly(tuple(range(10)), 10) -> повертає -1
 """
 
-def search_linearly(x, y):
-  if y in x:
-    return x.index(y)
-  else:
-    return -1
+def search_linearly(iterable, element):
+	for i in range(len(iterable)):
+		if iterable[i] == element:
+			return i	
+	return -1
+
+	# if y in x:
+	#   return x.index(y)
+	# else:
+	#   return -1
 
 
-search_linearly([1, 8, 7, 33, 9, 2], 8)
-search_linearly("hello world!", "!")
-search_linearly(tuple(range(10)), 10)
+print(search_linearly([1, 8, 7, 33, 9, 2], 8))
+print(search_linearly("hello world!", "!"))
+print(search_linearly(tuple(range(10)), 10))
