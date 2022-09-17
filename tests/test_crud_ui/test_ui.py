64e0a77b4a5969a34a731599a9869bcb754ce5c8
andrii_holub <andrii.holub@lembergsolutions.com>
2022-09-17 21:07:44 +0300

    Fixed tests naming

--- tests/test_crud_ui/test_ui.py
+++ tests/test_crud_ui/test_ui.py
@@ -1,10 +1,33 @@
 import pytest
 
+from src.main import TestUser
+
 
 @pytest.mark.usefixtures("login_logout")
-class TestAPI:
-    def test_api_1(self):
-        print('\ntesting ui.............')
+class TestUI:
 
-    def test_api_2(self):
-        print('\ntesting ui.............')
+    def test_create_user(self, driver):
+        user = TestUser(driver)
+        user.create_user(driver)
+        assert user.is_user_exist(driver, user.username)
+        user.delete_user(driver)
+
+    def test_check_user_info(self, driver):
+        user = TestUser(driver)
+        user.create_user(driver)
+        assert user.is_username_equal(driver)
+        assert user.join_date == user.get_user_join_time(driver)
+        user.delete_user(driver)
+
+    def test_edit_user_join_date(self, driver):
+        user = TestUser(driver)
+        user.create_user(driver)
+        user.edit_username(driver)
+        assert user.is_username_equal(driver)
+        user.delete_user(driver)
+
+    def test_delete_user(self, driver):
+        user = TestUser(driver)
+        user.create_user(driver)
+        user.delete_user(driver)
+        assert not user.is_user_exist(driver, user.username)
